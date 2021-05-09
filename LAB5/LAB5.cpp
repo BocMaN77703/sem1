5c8a7ef3d38aa00c02ba01898ab6ead2dc4638ee
BocMaN77703 <73021186+BocMaN77703@users.noreply.github.com>
2021-05-10 00:25:37 +0300

    Add files via upload

---
+++ LAB5/LAB5.cpp
@@ -0,0 +1,44 @@
+﻿//Номер 8
+#include <stdio.h>
+#include <locale.h>
+#include <malloc.h>
+int main() {
+	setlocale(LC_ALL, "rus"); //Русификация вывода
+	int i, j, n=0, m=0, min=0, k=0;
+	int** ms; 
+	printf("Введите количесво строк и столбцов:");
+	while (!scanf_s("%d%d", &n, &m)) {
+		printf("Вы ввели не число. Введите колличество строк и столбцов: "); //проверка на правильность ввода и ввод
+		rewind(stdin);
+	}
+	ms = (int**)malloc(n * sizeof(int)); //Выделение памяти
+	for (i = 0; i < n; i++)
+		ms[i] = (int*)malloc(m * sizeof(int));
+	printf("Введите элементы массива:");
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < m; j++) {
+			while (!scanf_s("%d", &ms[i][j])){
+				printf("Вы ввели не число. Введите элементы массива: ");
+				rewind(stdin);
+			}
+		}
+	}
+	min = ms[0][0];
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < m; j++) {
+			if (ms[i][j] <= min) //Определение минимального элемента
+				min = ms[i][j];
+		}
+	}
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < m; j++) {
+			if (ms[i][j]%min==0) //Проверка на кратность
+				k++;
+		}
+	}
+	printf("Количество элементов кратных минимальному: %d", k); //Вывод
+	for (int i = 0; i < n; i++)
+		free(ms[i]); //Очищение памяти
+	free(ms);
+	return 0;
+}
---
+++ LAB6/LAB6.cpp
@@ -0,0 +1,42 @@
+﻿//num 9
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+int main() {
+	double* ms;
+	double m=0;
+	int k=0, i=0;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите колличество элементов массива: ");
+	while (!scanf_s("%d", &k)) {
+		rewind(stdin);
+		printf("Вы ввели не число. Введите колличество элементов массива: ");
+	}
+	ms = (double*)malloc(k * sizeof(double));
+	printf("Введите элементы массива: ");
+	for (i = 0; i < k; i++) {
+		while (!scanf_s("%lf", &ms[i])) {
+			rewind(stdin);
+			printf("Вы ввели не число. Введите элемент массива: ");
+		}
+	}
+	m = ms[0];
+	if (ms[0] >= 0) {
+		for (i = 0; i < k; i++) {
+			if (ms[i] <= m)
+				m = ms[i];
+		}
+	}
+	else {
+		for (i = 0; i < k; i++) {
+			if (ms[i] >= m)
+				m = ms[i];
+		}
+	}
+	m = m * m;
+	printf("ИТОГОВАЯ ПОСЛДОВАТЕЛЬНОСТЬ:\n");
+	for (i = 0; i < k; i++)
+		printf("%.3lf ", ms[i]*m);
+	free(ms);
+	return 0;
+}
